195ac0c28964451dae1ddad82a9ba01e61a20af4
Yurii Popleta <yurka279@gmail.com>
2022-11-15 11:20:38 +0100

    Initial Commit

---
+++ Async_Image/Async_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_ImageApp.swift
+//  Async_Image
+//
+//  Created by Admin on 15/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Async_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
